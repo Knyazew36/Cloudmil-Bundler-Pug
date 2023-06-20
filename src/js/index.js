592957cc6e32f5ae3e75957645ecc4d294678aa4
Knyazew36 <knyazew.sv@gmail.com>
2023-06-21 00:13:01 +0300

    #up

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,34 @@
 import '../styles/style.scss';
 import 'virtual:svg-icons-register';
 import 'lazysizes';
-import uiAccordion from '../blocks/_ui/ui-accordion/ui-accordion';
-import uiDatepicker from '../blocks/_ui/ui-datepicker/ui-datepicker';
 import uiTextarea from '../blocks/_ui/ui-textarea/ui-textarea';
-import uiTooltip from '../blocks/_ui/ui-tooltip/ui-tooltip';
 import gallery from '../blocks/gallery/gallery';
 import slider from '../blocks/slider/slider';
-import tabs from '../blocks/tabs/tabs';
+
+import './libs/jquery.min';
+import './libs/main';
+import './libs/jquery-countTo';
+import './libs/jquery-validate';
+import './libs/jquery.cookie';
+import './libs/jquery.easing';
+// import './libs/parallax';
+import './libs/bootstrap.min';
+// import './libs/imagesloaded.min';
+import './libs/jquery.bxslider.min';
+import './libs/jquery.magnific-popup.min';
+import './libs/owl.carousel.min';
+import './libs/parallax';
+// import './libs/rev-slider';
+// import './libs/wow.min';
+// import './libs/animation';
+// import '../rev-slider/js/jquery.themepunch.revolution.min';
 
 document.addEventListener('DOMContentLoaded', function () {
-  uiAccordion();
-  uiDatepicker();
+  // uiAccordion();
+  // uiDatepicker();
   uiTextarea();
-  uiTooltip();
+  // uiTooltip();
   gallery();
   slider();
-  tabs();
+  // tabs();
 });
