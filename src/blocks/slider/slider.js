44c7fa8dc509c4354c47045dcb6936d56e4616d3
Knyazew36 <knyazew.sv@gmail.com>
2023-06-28 00:44:15 +0300

    #up

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,16 +1,16 @@
-import Swiper from 'swiper/bundle'
+import Swiper from 'swiper/bundle';
 
-export default function slider() {
-  const swiperSlider = new Swiper('.slider .swiper', {
-    loop: true,
-    spaceBetween: 100,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets'
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    }
-  })
-}
+// export default function slider() {
+//   const swiperSlider = new Swiper('.slider .swiper', {
+//     loop: true,
+//     spaceBetween: 100,
+//     pagination: {
+//       el: '.swiper-pagination',
+//       type: 'bullets'
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev'
+//     }
+//   })
+// }
--- src/js/libs/swiper.js
+++ src/js/libs/swiper.js
@@ -1,11 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('.preview-slider', {
     loop: true,
     autoplay: {
-      delay: 2000,
+      delay: 6500,
     },
-    slidesPerView: 'auto',
+    speed: 600,
+    // slidesPerView: 'auto',
     spaceBetween: 10,
     allowTouchMove: false,
+  });
+  swiper.on('slideNextTransitionStart', function () {
+    var currentSlide = swiper.slides[swiper.activeIndex];
+    var currentTitle = currentSlide.querySelector('.preview__title');
+    currentTitle.classList.remove('slider-active');
+  });
+  swiper.on('slideNextTransitionEnd', function () {
+    var currentSlide = swiper.slides[swiper.activeIndex];
+    var currentTitle = currentSlide.querySelector('.preview__title');
+    currentTitle.classList.add('slider-active');
   });
 });
