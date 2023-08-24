02315dcbd63d82350f09ea6d582297cb76b1578c
DESKTOP-FR5AHN7\knyaz <knyazew.sv@gmail.com>
2023-08-24 21:45:18 +0300

    1

--- src/blocks/swiper-bottom/swiper-bottom.js
+++ src/blocks/swiper-bottom/swiper-bottom.js
@@ -1,10 +1,9 @@
 export const swiperBottom = () => {
-  const swiper = new Swiper('[data-swiper="bottom"]', {
+  new Swiper('[data-swiper="bottom"]', {
     loop: true,
     autoplay: {
       delay: 4500,
     },
-
     speed: 600,
     slidesPerView: 'auto',
   });
